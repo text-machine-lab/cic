b145e42026bad4cde3e95dcede283141d2c5a466
David Donahue <djd1283@gmail.com>
2017-10-26 18:52:40 -0400

    Separate Cornell Movie Dialogue dataset sentences from the numpy conversion of those sentences. Move this conversion code into parent class which the CMDD class subclasses from. This parent class can be used for other string input datasets.

--- cornell_movie_dialogues.py
+++ cornell_movie_dialogues.py
@@ -8,59 +8,34 @@ import unittest2
 import squad_dataset_tools as sdt
 
 
-class CornellMovieDialoguesDataset(gmtk.Dataset):
-    def __init__(self, max_message_length=30, token_to_id=None, num_examples=None):
-        """Currently creates a dataset of utterances from the dataset, for use in training an autoencoder.
-        Does not return messsage -> response pairs as of now. max_message_length includes stop token, so really
-        the largest sentence the autoencoder can encode is one less than the max length!"""
-        self.stop_token = '<STOP>'
-        self.num_examples = num_examples
-        self.max_message_length = max_message_length
+class StringDataset(gmtk.Dataset):
+    def __init__(self, strings, max_length, token_to_id=None, stop_token='<STOP>'):
+        self.stop_token = stop_token
+        self.max_message_length = max_length
+        self.strings = strings
+        print(max_length)
         self.nlp = spacy.load('en_core_web_sm')
-        # Add -1 to message length to include stop token
-        self.messages = self._load_messages_from_cornell_movie_lines(config.CORNELL_MOVIE_LINES_FILE, self.nlp,
-                                                                     max_number_of_messages=self.num_examples,
-                                                                     max_message_length=self.max_message_length - 1,
-                                                                     stop_token=self.stop_token)
+
         # Create or reuse vocabulary
         if token_to_id is None:
-            self.token_to_id, self.id_to_token = create_vocabulary(self.messages)
+            self.token_to_id, self.id_to_token = create_vocabulary(self.strings)
+            vocab_size = len(self.token_to_id)
+            self.token_to_id[self.stop_token] = vocab_size
+            self.id_to_token[vocab_size] = self.stop_token
         else:
             self.token_to_id = token_to_id
             self.id_to_token = {v: k for k, v in token_to_id.items()}
 
-        self.np_messages = construct_numpy_from_messages(self.messages,
-                                                         self.token_to_id,
-                                                         self.max_message_length)
+        self.np_messages, self.formatted_and_filtered_strings = self.convert_strings_to_numpy(self.strings)
 
-    def _load_messages_from_cornell_movie_lines(self, movie_lines_filename, nlp, max_number_of_messages=None,
-                                               max_message_length=None, stop_token=None):
-        delimiter = ' +++$+++ '
-        movie_lines_file = open(movie_lines_filename, 'rb')
-        messages = []
-        line_index = 0
-        for message_line in movie_lines_file:
-            if max_number_of_messages is None or line_index < max_number_of_messages:
-                try:
-                    message_data = message_line.decode('utf-8').split(delimiter)
-                    message = message_data[4][:-1]
-                    tk_message = nlp.tokenizer(message.lower())
-                    tk_tokens = [str(token) for token in tk_message if str(token) != ' ']
-                    if stop_token is not None:
-                        tk_tokens += [stop_token]
-                    if max_message_length is None or len(tk_tokens) <= max_message_length:
-                        messages.append(tk_tokens)
-                        line_index += 1
-                except UnicodeDecodeError:
-                    pass
-            else:
-                break
-        return messages
+            # = construct_numpy_from_messages(self.strings,
+            #                                              self.token_to_id,
+            #                                              self.max_message_length)
 
     def convert_numpy_to_strings(self, np_messages):
         messages = sdt.convert_numpy_array_to_strings(np_messages, self.id_to_token,
-                                                                 self.stop_token,
-                                                                 keep_stop_token=False)
+                                                      self.stop_token,
+                                                      keep_stop_token=False)
         return messages
 
     def convert_strings_to_numpy(self, strings):
@@ -69,9 +44,11 @@ class CornellMovieDialoguesDataset(gmtk.Dataset):
             tk_string = self.nlp.tokenizer(each_string.lower())
             tk_tokens = [str(token) for token in tk_string if str(token) != ' ' and str(token) in self.token_to_id]
             tk_tokens += [self.stop_token]
-            tk_token_strings.append(tk_tokens)
-
-        return construct_numpy_from_messages(tk_token_strings, self.token_to_id, self.max_message_length)
+            if len(tk_tokens) < self.max_message_length:
+                tk_token_strings.append(tk_tokens)
+        np_messages = construct_numpy_from_messages(tk_token_strings, self.token_to_id, self.max_message_length)
+        formatted_and_filtered_strings = [' '.join(tk_token_string) for tk_token_string in tk_token_strings]
+        return np_messages, formatted_and_filtered_strings
 
     def get_vocabulary(self):
         return self.token_to_id, self.id_to_token
@@ -83,11 +60,134 @@ class CornellMovieDialoguesDataset(gmtk.Dataset):
         return {'message': self.np_messages[index]}
 
     def __len__(self):
-        return len(self.messages)
+        return len(self.np_messages)
+
+class CornellMovieDialoguesDataset(StringDataset):
+    def __init__(self, max_message_length=30, token_to_id=None, num_examples=None):
+        """Currently creates a dataset of utterances from the dataset, for use in training an autoencoder.
+        Does not return messsage -> response pairs as of now. max_message_length includes stop token, so really
+        the largest sentence the autoencoder can encode is one less than the max length!"""
+        self.num_examples = num_examples
+
+        stop_token = '<STOP>'
+
+        messages = self._load_messages_from_cornell_movie_lines(config.CORNELL_MOVIE_LINES_FILE,
+                                                                     max_number_of_messages=self.num_examples)
+
+        # for i in range(10):
+        #     print(messages[i])
+
+        super().__init__(messages, max_message_length, token_to_id=token_to_id, stop_token=stop_token)
+
+    def _load_messages_from_cornell_movie_lines(self, movie_lines_filename, max_number_of_messages=None):
+        delimiter = ' +++$+++ '
+        movie_lines_file = open(movie_lines_filename, 'rb')
+        messages = []
+        line_index = 0
+        for message_line in movie_lines_file:
+            if max_number_of_messages is None or line_index < max_number_of_messages:
+                try:
+                    message_data = message_line.decode('utf-8').split(delimiter)
+                    message = message_data[4][:-1]
+                    # tk_message = nlp.tokenizer(message.lower())
+                    # tk_tokens = [str(token) for token in tk_message if str(token) != ' ']
+                    # if stop_token is not None:
+                    #     tk_tokens += [stop_token]
+                    # if max_message_length is None or len(tk_tokens) <= max_message_length:
+                    #     messages.append(' '.join(tk_tokens))
+                    #     line_index += 1
+                    messages.append(message)
+                except UnicodeDecodeError:
+                    pass
+            else:
+                break
+
+        movie_lines_file.close()
+
+        return messages
+
+
+# class CornellMovieDialoguesDataset(gmtk.Dataset):
+#     def __init__(self, max_message_length=30, token_to_id=None, num_examples=None):
+#         """Currently creates a dataset of utterances from the dataset, for use in training an autoencoder.
+#         Does not return messsage -> response pairs as of now. max_message_length includes stop token, so really
+#         the largest sentence the autoencoder can encode is one less than the max length!"""
+#         self.stop_token = '<STOP>'
+#         self.num_examples = num_examples
+#         self.max_message_length = max_message_length
+#         self.nlp = spacy.load('en_core_web_sm')
+#         # Add -1 to message length to include stop token
+#         self.messages = self._load_messages_from_cornell_movie_lines(config.CORNELL_MOVIE_LINES_FILE, self.nlp,
+#                                                                      max_number_of_messages=self.num_examples,
+#                                                                      max_message_length=self.max_message_length - 1,
+#                                                                      stop_token=self.stop_token)
+#         # Create or reuse vocabulary
+#         if token_to_id is None:
+#             self.token_to_id, self.id_to_token = create_vocabulary(self.messages)
+#         else:
+#             self.token_to_id = token_to_id
+#             self.id_to_token = {v: k for k, v in token_to_id.items()}
+#
+#         self.np_messages = construct_numpy_from_messages(self.messages,
+#                                                          self.token_to_id,
+#                                                          self.max_message_length)
+#
+#     def _load_messages_from_cornell_movie_lines(self, movie_lines_filename, nlp, max_number_of_messages=None,
+#                                                max_message_length=None, stop_token=None):
+#         delimiter = ' +++$+++ '
+#         movie_lines_file = open(movie_lines_filename, 'rb')
+#         messages = []
+#         line_index = 0
+#         for message_line in movie_lines_file:
+#             if max_number_of_messages is None or line_index < max_number_of_messages:
+#                 try:
+#                     message_data = message_line.decode('utf-8').split(delimiter)
+#                     message = message_data[4][:-1]
+#                     tk_message = nlp.tokenizer(message.lower())
+#                     tk_tokens = [str(token) for token in tk_message if str(token) != ' ']
+#                     if stop_token is not None:
+#                         tk_tokens += [stop_token]
+#                     if max_message_length is None or len(tk_tokens) <= max_message_length:
+#                         messages.append(tk_tokens)
+#                         line_index += 1
+#                 except UnicodeDecodeError:
+#                     pass
+#             else:
+#                 break
+#         return messages
+#
+#     def convert_numpy_to_strings(self, np_messages):
+#         messages = sdt.convert_numpy_array_to_strings(np_messages, self.id_to_token,
+#                                                                  self.stop_token,
+#                                                                  keep_stop_token=False)
+#         return messages
+#
+#     def convert_strings_to_numpy(self, strings):
+#         tk_token_strings = []
+#         for each_string in strings:
+#             tk_string = self.nlp.tokenizer(each_string.lower())
+#             tk_tokens = [str(token) for token in tk_string if str(token) != ' ' and str(token) in self.token_to_id]
+#             tk_tokens += [self.stop_token]
+#             tk_token_strings.append(tk_tokens)
+#
+#         return construct_numpy_from_messages(tk_token_strings, self.token_to_id, self.max_message_length)
+#
+#     def get_vocabulary(self):
+#         return self.token_to_id, self.id_to_token
+#
+#     def get_stop_token(self):
+#         return self.stop_token
+#
+#     def __getitem__(self, index):
+#         return {'message': self.np_messages[index]}
+#
+#     def __len__(self):
+#         return len(self.messages)
 
 
 def create_vocabulary(messages):
-    token_to_id = gensim.corpora.Dictionary(documents=messages).token2id
+    message_tokens = [message.split() for message in messages]
+    token_to_id = gensim.corpora.Dictionary(documents=message_tokens).token2id
     id_to_token = {v: k for k, v in token_to_id.items()}
     # Add '' as index 0
     num_non_empty_words = len(token_to_id)
@@ -122,15 +222,18 @@ class CornellMovieDialoguesTest(unittest2.TestCase):
         cmd_dataset = CornellMovieDialoguesDataset()
 
         token_to_id, id_to_token = cmd_dataset.get_vocabulary()
-
-        for index in range(len(cmd_dataset)):
+        assert len(token_to_id) == len(id_to_token)
+        nlp = spacy.load('en_core_web_sm')
+        m = len(cmd_dataset)
+        assert m == len(cmd_dataset.formatted_and_filtered_strings)
+        for index in range(m):
             each_example = cmd_dataset[index]
             each_np_message = np.reshape(each_example['message'], newshape=(-1, 30))
             each_reconstructed_message = sdt.convert_numpy_array_to_strings(each_np_message, id_to_token,
                                                                             stop_token=cmd_dataset.stop_token,
                                                                             keep_stop_token=True)[0]
-            each_message = ' '.join(cmd_dataset.messages[index])
-            if len(cmd_dataset.messages[index]) <= 30:
+            each_message = cmd_dataset.formatted_and_filtered_strings[index]
+            if len(cmd_dataset.formatted_and_filtered_strings[index].split()) <= 30:
                 if each_message != each_reconstructed_message:
                     print(each_message)
                     print(each_reconstructed_message)
@@ -139,6 +242,7 @@ class CornellMovieDialoguesTest(unittest2.TestCase):
     def test_len(self):
         cmd_dataset = CornellMovieDialoguesDataset(max_message_length=10)
 
-        for each_message in cmd_dataset.messages:
-            assert len(each_message) <= 10
+        for index in range(cmd_dataset.np_messages.shape[0]):
+            assert cmd_dataset.np_messages[index, -1] == cmd_dataset.token_to_id[cmd_dataset.stop_token] or \
+                   cmd_dataset.np_messages[index, -1] == cmd_dataset.token_to_id['']
 
--- uk_wac.py
+++ uk_wac.py
@@ -1,8 +1,9 @@
 """UK WAC dataset."""
 import gmtk
+import cornell_movie_dialogues
 
-class UKWacDataset(gmtk.Dataset):
-    def __init__(self, ukwac_path, result_path=None, training_set=False, test_set=False):
+class UKWacDataset(cornell_movie_dialogues.StringDataset):
+    def __init__(self, ukwac_path, result_path=None, token_to_id=None, max_length=None, training_set=False, test_set=False):
         """Instantiate UK Wac dataset from raw ukwac_path file (slow). Store and load
         resulting training examples to and from result_path (fast). Separates dataset
         into training and testing sets and builds vocabulary. Must specify whether to
@@ -15,26 +16,34 @@ class UKWacDataset(gmtk.Dataset):
 
         self.ukwac_path = ukwac_path
 
+        def contains_numbers(s):
+            return any(c.isdigit() for c in s)
+
         filtered_sentences = []
         for index, line in enumerate(open(self.ukwac_path, 'r', encoding='utf-8', errors='ignore')):
-            if index < 4:
-                # print(line)
-                sentences = line.split(' . ')
-                for each_sentence in sentences:
-                    each_sentence = each_sentence.lower()
-                    #print(each_sentence)
-                    if not each_sentence.isspace() \
-                            and '(' not in each_sentence \
-                            and ')' not in each_sentence \
-                            and not each_sentence.startswith('current url'):
-                        each_sentence += '.'
-                        each_sentence = each_sentence.strip()
+            sentences = line.split(' . ')
+            for each_sentence in sentences:
+                each_sentence = each_sentence.lower()
+                #print(each_sentence)
+                if not each_sentence.isspace() \
+                        and '(' not in each_sentence \
+                        and ')' not in each_sentence \
+                        and not each_sentence.startswith('current url') \
+                        and not contains_numbers(each_sentence) \
+                        and '"' not in each_sentence \
+                        and ':' not in each_sentence:
+                    each_sentence += '.'
+                    each_sentence = each_sentence.strip()
+                    if max_length is None or len(each_sentence.split()) < max_length:
                         filtered_sentences.append(each_sentence)
-            else:
-                break
 
-        for filtered_sentence in filtered_sentences:
-            print(filtered_sentence)
+        super().__init__(filtered_sentences, max_length=max_length, token_to_id=token_to_id)
+
+        print(len(filtered_sentences))
+        for index in range(10):
+            print(filtered_sentences[index])
+            print(self.np_messages[index])
+
 
     def __getitem__(self, index):
         pass
@@ -47,4 +56,4 @@ if __name__ == '__main__':
     ukwac_path = '/data2/arogers/Corpora/En/UkWac/Plain-txt/ukwac_subset_100M.txt'
     result_path = './data/ukwac/'
     print('Loading dataset...')
-    ukwac = UKWacDataset(ukwac_path, result_path=result_path, training_set=True)
+    ukwac = UKWacDataset(ukwac_path, result_path=result_path, max_length=20, training_set=True)
